6b32cc59887c6d26e640994f7c0043118cac20b3
Ha Quang Hieu <you@example.comheguangxiao@gmail.com>
2019-05-29 10:33:55 +0700

    hien thi dau ra l3

---
+++ js.js
@@ -0,0 +1,15 @@
+function showbtap1() {
+    alert("Xin chao!");
+}
+
+function showbtap2(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+
+function showbtap3(){
+    document.write('Xin chào!');
+}
+
+function showbtap4(){
+    console.log('Xin chao !');
+}
